13eaab9c9b7522a84f7b3f56278f17144ac99b4b
Aditya2012-22 <adi.adi22b@gmail.com>
2026-01-18 16:13:01 +0530

    initial commit

---
+++ set union and intersection.py
@@ -0,0 +1,11 @@
+set1 = {"Green", "Blue"}
+set2 = {"Blue", "yellow"}
+print("Original sets:")
+print(set1)
+print(set2)
+set = set1.union(set2)
+print("\nUniion of above sets:")
+print(set)
+print("\nIntersection of two said sets:")
+set3 = set1.intersection(set2)
+print(set3)
